7b222ba58ec5b490384caca446b72444a6d42160
mariantirlea <mariantirlea@hotmail.com>
2020-12-22 15:28:06 +0200

    Add ProxyHelper class

---
+++ stp-core/src/main/java/ro/mariantirlea/sockettrafficproxy/stpcore/ProxyHelper.java
@@ -0,0 +1,12 @@
+package ro.mariantirlea.sockettrafficproxy.stpcore;
+
+public class ProxyHelper {
+
+    public static void main(String[] args) {
+
+        System.out.println("Proxy was started!");
+
+    }
+
+}
+
