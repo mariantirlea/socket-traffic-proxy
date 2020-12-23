32b8139883e6f79b417a1613ff049bb597922b0f
mariantirlea <mariantirlea@hotmail.com>
2020-12-24 00:06:34 +0200

    Refactoring and remove useless code

--- stp-core/src/main/java/ro/mariantirlea/sockettrafficproxy/stpcore/ProxyHelper.java
+++ stp-core/src/main/java/ro/mariantirlea/sockettrafficproxy/stpcore/ProxyHelper.java
@@ -39,24 +39,6 @@ public class ProxyHelper extends Thread {
 //        serverSocket.close();
 //    }
 
-    public static String readInputStream(BufferedInputStream _in)
-            throws IOException {
-        String data = "";
-        int s = _in.read();
-        if(s==-1)
-            return null;
-        data += ""+(char)s;
-        int len = _in.available();
-        System.out.println("Len got : "+len);
-        if(len > 0) {
-            byte[] byteData = new byte[len];
-            _in.read(byteData);
-            data += new String(byteData);
-        }
-
-//        System.err.println(data);
-        return data;
-    }
 }
 
 class GreetClient {
@@ -66,152 +48,122 @@ class GreetClient {
 
     public void startConnection(String ip, int port) throws IOException {
         clientSocket = new Socket(ip, port);
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedInputStream(clientSocket.getInputStream());
     }
 
-    public String sendMessage(String msg) throws IOException {
-//        System.err.println(msg);
-        out.println(msg);
-        return ProxyHelper.readInputStream(in);
-    }
-
-    public void stopConnection() throws IOException {
-        in.close();
-        out.close();
-        clientSocket.close();
-    }
 }
 
 class EchoClientHandler{
-    private Socket clientSocket;
+    private Socket fromSocket;
     private String destinationIP;
     private int remotePort;
-    private PrintWriter out;
-    private BufferedInputStream in;
+
 
     public EchoClientHandler(Socket socket, String destinationIP, int remotePort) {
-        this.clientSocket = socket;
+        this.fromSocket = socket;
         this.destinationIP = destinationIP;
         this.remotePort = remotePort;
     }
 
-    public synchronized void run() throws InterruptedException {
+    public void run() throws InterruptedException {
         System.out.println("New client was connected..." +  Thread.currentThread().getName() + Thread.currentThread().getId());
 
         try {
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedInputStream(clientSocket.getInputStream());
+            BufferedInputStream fromSocketInput = new BufferedInputStream(fromSocket.getInputStream());
+            BufferedOutputStream fromSocketOutput = new BufferedOutputStream(fromSocket.getOutputStream());
 
+            Socket toSocket = new Socket(destinationIP, remotePort);
+            BufferedInputStream toSocketInput = new BufferedInputStream(toSocket.getInputStream());
+            BufferedOutputStream toSocketOutput = new BufferedOutputStream(toSocket.getOutputStream());
 
-            Socket clientSocket = new Socket(destinationIP, remotePort);
-            PrintWriter out2 = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedInputStream in2 = new BufferedInputStream(clientSocket.getInputStream());
-            clientSocket.setSoTimeout(10000);
-            this.clientSocket.setSoTimeout(10000);
+            fromSocket.setSoTimeout(100);
+            toSocket.setSoTimeout(100);
 
-            byte[] buffer = new byte[4096];
-            int read;
+            Thread t1 = new Thread(() -> {
+                Thread.currentThread().setName("fromSocket" + Thread.currentThread().getId());
 
-            if(in.available() > 0){
-                do {
+                byte[] buffer = new byte[4096];
 
-
-                    read = in.read(buffer);
-                    if (read > 0) {
-                        clientSocket.getOutputStream().write(buffer, 0, read);
-//                        System.err.println(new String(buffer));
-                        if (in.available() < 1) {
-                            clientSocket.getOutputStream().flush();
+                try {
+                    do {
+                        if(fromSocketInput.available() <=0 ){
+                            System.out.println("Nothing to read from: " + Thread.currentThread().getId());
+                            break;
                         }
-                    }else{
-                        clientSocket.getOutputStream().flush();
-                    }
-                } while (in.available() > 0);
+                        int size = fromSocketInput.read(buffer);
 
-                byte[] buffer2 = new byte[4096];
-                int read2;
-                boolean first = true;
-                do {
-                    read2 = in2.read(buffer2);
-                    if (read2 > 0) {
-//                        System.err.println(new String(buffer2));
-                        Thread.sleep(10);
-                        this.clientSocket.getOutputStream().write(buffer2, 0, read2);
-                        if (in2.available() < 1) {
+                        if(size > 0){
+//                            System.out.println("Read input from: " + Thread.currentThread().getId());
 
-                            if(first){
-                                Thread.sleep(50);
-                                first = false;
-                                continue;
+                            toSocketOutput.write(buffer, 0, size);
+                            toSocketOutput.flush();
+//                            System.err.println(size);
+//                            System.err.println(new String(buffer));
+                        }
+
+                    }while(fromSocketInput.available() > 0);
+
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+                System.out.println("Try to close FROM thread: " + Thread.currentThread().getId());
+
+            });
+            t1.start();
+
+            Thread t2 = new Thread(() -> {
+                Thread.currentThread().setName("toSocket" + Thread.currentThread().getId());
+
+                byte[] buffer = new byte[4096];
+
+                try {
+                    do {
+                        int size = 0;
+
+                        try {
+                            size = toSocketInput.read(buffer);
+
+                            if(size > 0){
+//                                System.out.println("Write input to: " + Thread.currentThread().getId());
+
+                                fromSocketOutput.write(buffer, 0, size);
+                                fromSocketOutput.flush();
+//                                System.err.println(size);
+//                            System.err.println(new String(buffer));
+                            }else{
+                                break;
                             }
 
-                            this.clientSocket.getOutputStream().flush();
+                        }catch (IOException e){
+                            System.out.println("Timeout -> stop an close");
+                            break;
 
                         }
-                    }else{
-                        this.clientSocket.getOutputStream().flush();
+
+
+                    }while(true);
+
+
+                }finally {
+                    try {
+                        System.out.println("Try to close TO thread: " + Thread.currentThread().getId());
+
+                        fromSocketInput.close();
+                        toSocketInput.close();
+                        fromSocketOutput.close();
+                        toSocketOutput.close();
+                        toSocket.close();
+                        fromSocket.close();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                } while (in2.available() > 0);
-            }
+                }
 
-            this.in.close();
-            this.out.close();
-            this.clientSocket.close();
+            });
+            t2.start();
 
-            in2.close();
-            out2.close();
-            clientSocket.close();
-
-
-//                in.close();
-//                clientSocket.close();
-
-
-
-//                String rec=null;
-//                while(true) {
-//                    rec=null;
-//                    try	{
-//                        rec = readInputStream(in);//in.readLine();
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                        break;
-//                    }
-//
-//                    if (rec != null) {
-//                        System.err.println(rec);
-//
-//                        //Send body to destination and return response from there
-//                        GreetClient client = new GreetClient();
-//                        client.startConnection("127.0.0.1", 8080);
-//                        Thread.sleep(2000);
-//                        out.write(client.sendMessage(rec));
-//
-////                        out.write("HTTP/1.1 200 \n" +
-////                                "Content-Type: text/html;charset=utf-8\n" +
-////                                "Content-Language: en\n" +
-////                                "Content-Length: 10\n" +
-////                                "Date: Tue, 22 Dec 2020 14:03:56 GMT\n" +
-////                                "Connection: close\n" +
-////                                "\n" +
-////                                "<!doctype");
-//                        out.write("");
-//                        out.flush();
-//                        clientSocket.close();
-//                        client.stopConnection();
-//                        //rec = rec.replaceAll("\n","<LF>");
-//                        //rec = rec.replaceAll("\r","<CR>");
-//                        //parent.append("R: "+rec);
-//                    } else {
-//
-//                        break;
-//                    }
-//                } //end of while
-
-//                in.close();
-//                out.close();
-//                clientSocket.close();
+            t1.join();
+            t2.join();
 
         } catch (IOException  e) {
             e.printStackTrace();
--- stp-core/src/main/java/ro/mariantirlea/sockettrafficproxy/stpcore/Runner.java
+++ stp-core/src/main/java/ro/mariantirlea/sockettrafficproxy/stpcore/Runner.java
@@ -9,6 +9,8 @@ public class Runner {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         new ProxyHelper(7090, "192.168.1.67", 7090).start();
+        new ProxyHelper(8508, "192.168.1.67", 8508).start();
+        new ProxyHelper(8509, "192.168.1.67", 8509).start();
 
     }
 
